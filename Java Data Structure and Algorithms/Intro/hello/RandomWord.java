52d90d12f423545c4c798e72e9ac3e64c5f88850
sanjay <rockysanjay287@gmail.com>
2023-10-31 22:38:28 +0530

    started DS and Algo

---
+++ Java Data Structure and Algorithms/Intro/hello/RandomWord.java
@@ -0,0 +1,22 @@
+package Intro.hello;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+    String challenger="";
+    String champ=StdIn.readString();
+    int count=1;
+
+    while(!StdIn.isEmpty()){
+        count++;
+        challenger=StdIn.readString();
+        if(StdRandom.bernoulli((1.0/count))){
+            champ=challenger;
+        }
+    }
+        StdOut.println(champ);
+    }
+}
